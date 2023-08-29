39ce2e0938d59118fd47aadb45f45ac2fb252d41
Bernese-Corgi <jnyng0621@gmail.com>
2023-08-29 19:25:30 +0900

    Feat: 로드맵 노드 api 초안

--- app/router/main.py
+++ app/router/main.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter
 
 
-from app.router import post, monitoring
+from app.router import post, monitoring, roadmap
 
 
 router = APIRouter()
@@ -10,6 +10,11 @@ router.include_router(
     router=monitoring.router,
     prefix="/monitoring",
     tags=["monitoring"]
+)
+router.include_router(
+    router=roadmap.router,
+    prefix="/roadmap",
+    tags=["roadmap"]
 )
 router.include_router(
     router=post.router,
---
+++ app/router/roadmap.py
@@ -0,0 +1,12 @@
+
+from fastapi import APIRouter
+
+
+router = APIRouter()
+# TODO 로드맵 생성 API
+
+# TODO 로드맵 노드 생성 API
+
+# TODO 로드맵 노드 이름 수정 API
+
+# TODO 로드맵 parent 수정 API
