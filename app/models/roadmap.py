9c78e5216dfd9b0d029695292d6f8404721bb53f
Bernese-Corgi <jnyng0621@gmail.com>
2023-08-29 19:22:32 +0900

    Feat: Roadmap, RoadmapNode 모델 추가 & alembic 업데이트

---
+++ app/models/roadmap.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from sqlalchemy import ForeignKey
+from app.models.base import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+class Roadmap(Base):
+    __tablename__ = "roadmap"
+    
+    roadmap_id: Mapped[int] = mapped_column("roadmap_id", primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column("title")
+    nodes: Mapped[List["RoadmapNode"]] = relationship(back_populates="roadmap")
+    
+    
+class RoadmapNode(Base):
+    __tablename__ = "roadmap_node"
+    
+    node_id: Mapped[int] = mapped_column("node_id", primary_key=True, autoincrement=True)
+    roadmap_id: Mapped[int] = mapped_column(ForeignKey("roadmap.roadmap_id"))
+    roadmap: Mapped["Roadmap"] = relationship(back_populates="nodes")
+    title: Mapped[str] = mapped_column("title")
+    parent_id: Mapped[int] = mapped_column(ForeignKey("roadmap_node.node_id"))
+    children = relationship("RoadmapNode", back_populates="parent")
+    parent = relationship("RoadmapNode", back_populates="children", remote_side=[node_id])
+
+    
