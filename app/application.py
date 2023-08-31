31a5e80c4d9d3c14653aa0970c481a5914e9089c
Bernese-Corgi <jnyng0621@gmail.com>
2023-09-01 01:24:51 +0900

    Feat: CORS 설정

--- app/application.py
+++ app/application.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from app.lifespan import lifespan
 from app.router import router
@@ -9,6 +11,20 @@ def app() -> FastAPI:
         lifespan=lifespan,
         docs_url="/api/docs",
         redoc_url="/api/redoc",
+    )
+    
+    app.add_middleware(GZipMiddleware, minimum_size=1000)
+    origins = [
+        "http://localhost",
+        "http://localhost:8080",
+        "http://localhost:3000",
+    ]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     app.include_router(
