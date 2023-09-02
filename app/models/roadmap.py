0ff5eef4574ca37309c980a7609d5473d09d8ad5
Bernese-Corgi <jnyng0621@gmail.com>
2023-09-02 17:30:04 +0900

    Feat: order_in_parent 필드 추가 - 노드 순서를 제어하기 위함

--- app/models/roadmap.py
+++ app/models/roadmap.py
@@ -20,7 +20,8 @@ class RoadmapNode(Base):
     roadmap: Mapped["Roadmap"] = relationship(back_populates="nodes")
     title: Mapped[str] = mapped_column("title")
     parent_id: Mapped[int] = mapped_column(ForeignKey("roadmap_node.node_id"), nullable=True)
-    children = relationship("RoadmapNode", back_populates="parent")
-    parent = relationship("RoadmapNode", back_populates="children", remote_side=[node_id])
+    children: Mapped[List["RoadmapNode"]] = relationship("RoadmapNode", back_populates="parent")
+    parent: Mapped["RoadmapNode"] = relationship("RoadmapNode", back_populates="children", remote_side=[node_id])
+    order_in_parent: Mapped[int] = mapped_column("order_in_parent", default=0)
 
     
